508df03ba60e7931f8a784988768f10749176eaf
vasilisa <vasilisaskv@gmail.com>
2020-11-26 09:47:10 +0100

    first commit

---
+++ routes/__init__.py
@@ -0,0 +1,9 @@
+import routes.error_handlers
+# import routes.games
+# import routes.game_blocks
+import routes.participants_question_data
+# import routes.participants_data
+# import routes.participants_data_bonus
+# import routes.participants_game
+import routes.attempts 
+import routes.config_data
